b7b326a812ebc373a3c7bd34b15d8e3c74cdc5ab
hashep <gooboo712@gmail.com>
2025-07-21 22:50:17 +0900

    hgh

---
+++ 10950.py
@@ -0,0 +1,5 @@
+c = int(input())
+
+for i in range(c):
+    A, B = map(int, input().split())
+    print(A + B)
---
+++ 25314.py
@@ -0,0 +1,6 @@
+N = int(input())
+A = N // 4
+
+result = "long " * (A) + "int"
+
+print(result)
